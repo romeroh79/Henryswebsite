d2992e8e71a1c7f40272915538baad2ec304fed3
romeroh79 <75192496+romeroh79@users.noreply.github.com>
2020-11-28 22:51:06 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,14 @@
+//Henry Romero 990419960
+function animebutton() {
+	var ab=document.getElementById("animebutton");
+	ab.style.display = "none";
+	document.getElementById("animetable").style.display = "block";
+	document.getElementById("abd").style.backgroundImage ="url(anime.gif)";
+}
+function hobbybutton() {
+	var hb=document.getElementById("hobbybutton");
+	hb.style.display = "none";
+	document.getElementById("hobbytable").style.display = "block";
+	document.getElementById("hbd").style.backgroundImage ="url(bridge.gif)";
+	document.getElementById("botb").style.display = "block";
+}
